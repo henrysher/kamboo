7a998df51b9812cb4488fe770942d8245021a2ea
Henry Huang <henry.s.huang@gmail.com>
2014-01-05 12:40:22 +0800

    Integrate with travis-ci

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup, find_packages
+
+import kamboo
+
+
+requires = ['boto3']
+
+if sys.version_info[:2] == (2, 6):
+    # For python2.6 we have to require argparse since it
+    # was not in stdlib until 2.7.
+    requires.append('argparse>=1.1')
+
+
+setup_options = dict(
+    name='kamboo',
+    version=kamboo.__version__,
+    description='',
+    long_description=open('README.rst').read(),
+    author='Henry Huang',
+    author_email='henry.s.huang@gmail.com',
+    url='https://github.com/henrysher/kamboo',
+    scripts=[],
+    packages=find_packages('.', exclude=['tests*']),
+    package_dir={'kamboo': 'kamboo'},
+    package_data={'kamboo': []},
+    install_requires=requires,
+    license="Apache License 2.0",
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ),
+)
+
+setup(**setup_options)
