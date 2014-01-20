fcd45fe909357783bb22cc4b7e11cd6db6ade4a7
Henry Huang <henry.s.huang@gmail.com>
2014-01-20 10:14:12 +0800

    Add exceptions for kamboo

---
+++ kamboo/exceptions.py
@@ -0,0 +1,17 @@
+class KambooException(Exception):
+    """
+    A base exception class for all Kamboo-related exceptions.
+    """
+    pass
+
+
+class ValidationError(KambooException):
+    pass
+
+
+class TooManyRecordsException(KambooException):
+    """
+    Exception raised when a search of records returns more
+    records than requested.
+    """
+    pass
