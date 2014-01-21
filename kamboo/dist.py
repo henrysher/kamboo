eb40e92be72579a9d8dab6f1467e3279f09d003b
Henry Huang <henry.s.huang@gmail.com>
2014-01-21 14:11:38 +0800

    Implement the distributee of single resource per region and account

--- kamboo/dist.py
+++ kamboo/dist.py
@@ -14,6 +14,104 @@
 
 
 import logging
-import kotocore
 
 log = logging.getLogger(__name__)
+
+
+class Distee(object):
+    """
+    Distribute your AWS resource per region and account
+    """
+    def __init__(self, type=None,
+                 source_account_id=None,
+                 source_credentials=None,
+                 source_region=None,
+                 source_id=None,
+                 dest_account_id=None,
+                 dest_credentials=None,
+                 dest_region=None,
+                 dest_tags=None):
+
+        self.source_account_id = source_account_id
+        self.source_credentials = source_credentials
+        self.source_region = source_region
+        self.source_id = source_id
+        self.dest_account_id = dest_account_id
+        self.dest_credentials = dest_credentials
+        self.dest_region = dest_region
+        self.dest_tags = dest_tags
+
+        module_name = '.'.join(["kamboo", type.lower()])
+        resource_name = str(type).title()
+        collection_name = ''.join([resource_name, "Collection"])
+
+        module = __import__(module_name, fromlist=[type.lower()])
+        self.collection = getattr(module, collection_name)
+        self.resource = getattr(module, resource_name)
+
+        self.src_collection = self.collection(region_name=source_region,
+                                              account_id=source_account_id,
+                                              credentials=source_credentials)
+
+        self.dest_collection = self.collection(region_name=source_region,
+                                               account_id=source_account_id,
+                                               credentials=source_credentials)
+
+        self.source = self.resource(source_id, collection=self.src_collection)
+
+    def distribute(self, wait=False):
+        """
+        Distribute the resource
+        """
+
+        if self.source_account_id == self.dest_account_id:
+            self.dist_to_same_account(wait=wait)
+        else:
+            self.dist_to_different_account(wait=wait)
+
+    def dist_to_same_account(self, collection=None, wait=False):
+        """
+        Distribute the resource to the same account
+        """
+        if collection is None:
+            collection = self.dest_collection
+
+        if wait is True:
+            new_resource = collection.wait_to_copy_resource(
+                self.source_region, self.source_id)
+        else:
+            new_resource = collection.copy_resource(
+                self.source_region, self.source_id)
+
+        if self.dest_tags:
+            new_resource.tags = tags
+        else:
+            new_resource.tags = self.source.tags
+
+        return new_resource
+
+    def dist_to_different_account(self, wait=False):
+        """
+        Distribute the resource to a different account
+        """
+
+        if self.source_region == self.dest_region:
+            self.source.add_permission(self.dest_account_id)
+            new_resource = self.source
+        else:
+            collection = self.collection(region_name=self.dest_region,
+                                         account_id=self.source_account_id,
+                                         credentials=self.source_credentials)
+
+            new_resource = self.dist_to_same_account(
+                collection=collection, wait=wait)
+            new_resource.add_permission(self.dest_account_id)
+
+        self.resource(new_resource.id, collection=self.dest_collection)
+
+        if self.dest_tags:
+            new_resource.tags = self.dest_tags
+        else:
+            new_resource.tags = self.source.tags
+
+        return new_resource
