7082db4f91a9e1444a58729cdbbd6a61a2699608
Henry Huang <henry.s.huang@gmail.com>
2014-01-21 14:10:31 +0800

    Add the license and update the timeout value

--- kamboo/utils.py
+++ kamboo/utils.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2014, Henry Huang
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import time
 from six.moves import range
 
@@ -60,9 +74,9 @@ def compare_list_of_dict(src_list, dest_list):
 
 
 def wait_to_complete(resource=None, expected_status=None,
-                     unit=5, timeout=60):
+                     unit=5, timeout=300):
     """
-    Wait for the specified operation to complete
+    Wait the specified resource to complete
     """
     for i in range(int(timeout/unit)):
         if resource.status == expected_status:
